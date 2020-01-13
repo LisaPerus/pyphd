1578e50ccbe76c877ddbb340179f81208273bb0b
LisaPerus <lisa.perus@etu.umontpellier.fr>
2020-01-13 15:03:50 +0100

    Added constants path for Conn analyses

--- pyphd/constants.py
+++ pyphd/constants.py
@@ -122,3 +122,27 @@ SCIMAGOJR_RANKING_DATA = {
     "link" : "https://www.scimagojr.com/journalrank.php?out=xls",
     "version" : "2018"
 }
+
+CONN_INPUTS = {
+    "M0" : {
+        "conn_file_pattern" : "resultsROI_*_Condition001.mat",
+        "datafile" : "/home/lp259104/PHD/NOTES/Conn_input_Montpellier_Bordeaux_Toulouse_MAPT_M0_only.csv",
+        "outdir" : "/home/lp259104/PHD/DATA/RESULTS/MAPT/CONNECTIVITY_ANALYSIS/SBC_01_Grecius_PRE/CONN_FIRST_LEVEL_CONNECTIVITY_RESULTS/PARAMETRIC_TESTS",
+        "conn_datapath" : "/media/lp259104/10086bbb-e609-4274-a6b1-fcb6e9e0f9cb/tmp/MAPT_Conn_Montpellier_Bordeaux_Toulouse_M0/conn_analysis/results/firstlevel/SBC_01_Grecius_PRE",
+        "timepoint_name" : "all_subjects_at_PRE"
+            },
+    "M36" : {
+        "conn_file_pattern" : "resultsROI_*_Condition001.mat",
+        "datafile" : "/home/lp259104/PHD/NOTES/Conn_input_Montpellier_Bordeaux_Toulouse_MAPT_M36_only.csv",
+        "outdir" : "/home/lp259104/PHD/DATA/RESULTS/MAPT/CONNECTIVITY_ANALYSIS/SBC_01_Grecius_POST/CONN_FIRST_LEVEL_CONNECTIVITY_RESULTS/PARAMETRIC_TESTS",
+        "conn_datapath" : "/media/lp259104/10086bbb-e609-4274-a6b1-fcb6e9e0f9cb/tmp/MAPT_Conn_Montpellier_Bordeaux_Toulouse_M36/conn_analysis/results/firstlevel/SBC_01_Grecius_M36",
+        "timepoint_name" : "all_subjects_at_POST"
+        },
+    "M36-M0" : {
+        "conn_file_pattern" : "zscores_resultsROI_*Condition001*_Condition002.mat",
+        "datafile" : "/home/lp259104/PHD/NOTES/Conn_input_Montpellier_Bordeaux_Toulouse_MAPT.csv",
+        "outdir" : "/home/lp259104/PHD/DATA/RESULTS/MAPT/CONNECTIVITY_ANALYSIS/SBC_04_Grecius_R50/CONN_FIRST_LEVEL_CONNECTIVITY_RESULTS/PARAMETRIC_TESTS",
+        "conn_datapath" : "/media/lp259104/10086bbb-e609-4274-a6b1-fcb6e9e0f9cb/tmp/MAPT_Conn_Montpellier_Bordeaux_Toulouse/conn_analysis/results/firstlevel/SBC_04_Grecius_R50/Condition002-Condition001",
+        "timepoint_name" : "zscored_diff_POST-PRE"
+        }
+}
